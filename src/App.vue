57e16ea90fe5794e3230e153aef65310cfd937ad
DBL117 <1141846055@qq.com>
2021-05-01 16:06:30 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    我的App
+    <Hello />
+  </div>
+</template>
+
+<script>
+import Hello from "./components/Hello";
+export default {
+  name: "App",
+  components: {
+    Hello,
+  },
+};
+</script>
+
+<style scope>
+</style>
